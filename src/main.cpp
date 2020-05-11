ab973d3a07d568c27715206774a7fbf610ea2c3b
piotex <pkubon2@gmail.com>
2020-05-11 13:21:51 +0200

    poprawa maina

--- src/main.cpp
+++ src/main.cpp
@@ -15,25 +15,25 @@ using std::cout;
 using std::endl;
 
 
-char getch() {
-        char buf = 0;
-        struct termios old = {0};
-        if (tcgetattr(0, &old) < 0)
-                perror("tcsetattr()");
-        old.c_lflag &= ~ICANON;
-        old.c_lflag &= ~ECHO;
-        old.c_cc[VMIN] = 1;
-        old.c_cc[VTIME] = 0;
-        if (tcsetattr(0, TCSANOW, &old) < 0)
-                perror("tcsetattr ICANON");
-        if (read(0, &buf, 1) < 0)
-                perror ("read()");
-        old.c_lflag |= ICANON;
-        old.c_lflag |= ECHO;
-        if (tcsetattr(0, TCSADRAIN, &old) < 0)
-                perror ("tcsetattr ~ICANON");
-        return (buf);
-}
+// char getch() {
+//         char buf = 0;
+//         struct termios old = {0};
+//         if (tcgetattr(0, &old) < 0)
+//                 perror("tcsetattr()");
+//         old.c_lflag &= ~ICANON;
+//         old.c_lflag &= ~ECHO;
+//         old.c_cc[VMIN] = 1;
+//         old.c_cc[VTIME] = 0;
+//         if (tcsetattr(0, TCSANOW, &old) < 0)
+//                 perror("tcsetattr ICANON");
+//         if (read(0, &buf, 1) < 0)
+//                 perror ("read()");
+//         old.c_lflag |= ICANON;
+//         old.c_lflag |= ECHO;
+//         if (tcsetattr(0, TCSADRAIN, &old) < 0)
+//                 perror ("tcsetattr ~ICANON");
+//         return (buf);
+// }
 
 int main(int argc, char **argv)
 {
@@ -53,7 +53,8 @@ int main(int argc, char **argv)
         while(1){
                 double tab[] = {0,0,0};
                 double kat[] = {0,0,0};
-                char znak = getch();
+                char znak ='x';//= getch();
+                std::cin >>znak ;
                 switch (znak)
                 {
                         case '3':tab[0] = predkosc; break;
