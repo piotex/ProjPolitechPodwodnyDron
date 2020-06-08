d76ce0e1f7f416f868d7e0ce166ad3bcdfc4bbde
piotex <pkubon2@gmail.com>
2020-06-08 13:40:32 +0200

    gotowe, pytanie tylko czemu usuwa wiecej wektorow niz tworzy ?

--- src/MacierzKw_temp.cpp
+++
@@ -1,5 +0,0 @@
-#include "../src/MacierzKw.cpp"
-
-template class MacierzKw<double,ROZMIAR>;
-template std::istream & operator>>(std::istream &strm, MacierzKw<double,ROZMIAR> &W);
-template std::ostream & operator<< (std::ostream &strm, const MacierzKw<double,ROZMIAR> &W);
--- src/Wektor_temp.cpp
+++
@@ -1,8 +0,0 @@
-#include "Wektor.cpp"
-
-typedef double TYP;
-
-template class Wektor<TYP,ROZMIAR>;
-template std::istream & operator>>(std::istream &strm, Wektor<TYP,ROZMIAR> &W);
-template std::ostream & operator<< (std::ostream &strm, const Wektor<TYP,ROZMIAR> &W);
-
